a70abd3bcf7b70b5851b571f0a26ee121ce00ebc
Pi-pizhu <812514981@qq.com>
2021-01-08 16:41:03 +0800

    添加获取用例根目录方法；添加初始化globalconfig实例，获取通用全局配置信息方法

--- us_interface/base/base_api.py
+++ us_interface/base/base_api.py
@@ -1,21 +1,41 @@
 import json
+import os
 import re
+import sys
 from urllib import parse
 import requests
 from string import Template
 from us_interface.config.global_config import GlobalConfig
 from loguru import logger
 from jsonpath import jsonpath
-
+from us_interface.utils.file_operations import path_extract, locate_file
 
 _url_compile = re.compile(r"^((2[0-4]\d|25[0-5]|[01]?\d\d?)\.){3}(2[0-4]\d|25[0-5]|[01]?\d\d?)$")
-_hosts_compile = re.compile(r"^((http://)|(https://))?([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}(/)")
+_hosts_compile = re.compile(
+    r"^((http://)|(https://))?([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}(/)")
+
 
 class BaseApi:
     _gl_config = GlobalConfig("/Users/yewenkai/PycharmProjects/HogwartsSDET14/us_interface/examples")
     _request_way = "https" or _gl_config.get_global_variable("env", "request_way")
     _domain_name = _gl_config.get_global_variable("hosts", "domain_name")
     _url = _gl_config.get_global_variable("hosts", _gl_config.get_global_variable("hosts", "default"))
+
+    def _get_test_root_path(self):
+        # 获取用例的根目录
+        work_path = os.getcwd()
+        test_paht = sys.argv[1]
+        original_path = os.path.join(work_path, test_paht)
+        # 用例的绝对路径
+        work_path = path_extract(original_path)
+        # test用例根目录
+        test_root_dir = locate_file(work_path, "env_config.ini")
+        return test_root_dir
+
+    def _get_global_config(self, test_root_dir):
+        # 获取全局配置信息config
+        self._gl_config = GlobalConfig(test_root_dir)
+        self.gl_config_data = self._gl_config.config_var
 
     def request_http(self, req_data):
         req_data = json.loads(req_data)
@@ -63,8 +83,3 @@ class BaseApi:
 
     def jsonpath(self, data, expr):
         return jsonpath(data, expr)
-
-
-if __name__ == '__main__':
-    a = BaseApi()
-    print(a.replace_hosts("https://qyapi.weixin.qq.com/cgi-bin/oa/schedule/add"))
