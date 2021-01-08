56c57e95fc15ca9508e6cdee9143aa41484f94b7
Pi-pizhu <812514981@qq.com>
2021-01-08 16:39:05 +0800

    添加初始化实例时加载去全局通用配置信息

--- us_interface/config/global_config.py
+++ us_interface/config/global_config.py
@@ -2,11 +2,30 @@
 # 单例加载全局配置 -> 提供访问
 import os
 
-from us_interface.utils.load_file import load_ini
+from us_interface.utils.file_operations import load_ini
+
+"""
+全局配置信息
+全局数据替换场景
+
+接口参数替换
+
+url替换
+
+局部变量：
+    一般是有接口依赖时，从上一个接口提取，传入teststep进行接口参数替换
+    这个不用管了
+    
+优先级：局部变量>全局配置
+从$ 或 ${} 中提取变量名
+匹配局部变量
+    匹配局部变量 
+    剩余的再匹配全局配置
+    如果还有剩 则记录 警告 但不报错
+"""
 
 
 class GlobalConfig:
-
     _first_init = False
 
     def __new__(cls, *args, **kwargs):
@@ -19,6 +38,7 @@ class GlobalConfig:
             work_path = work_path or os.getcwd()
             self._config_path = os.path.join(work_path, "env_config.ini")
             self._get_ini_instance(self._config_path)
+            self.config_var = self._get_config_var()
             self._global_config = {}
             self._first_init = True
 
@@ -36,3 +56,15 @@ class GlobalConfig:
             except Exception as error:
                 err_msg = f"faile load variable section:{section} option:{option}\n"
                 err_msg += str(error)
+
+    def _get_config_options(self):
+        # 获取全局配置的信息名称
+        return self._ini_instance.options("config")
+
+    def _get_config_var(self) -> dict:
+        # 获取全局配置信息
+        options = self._get_config_options()
+        config_var = {}
+        for option in options:
+            config_var[option] = self.get_global_variable("config", option)
+        return config_var
